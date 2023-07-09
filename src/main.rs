8cabba3360eabb56d8cda29e17bed0000ffe97ff
iDeBugger Freeman <me@idebugger.io>
2023-07-09 20:35:41 +0200

    Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use crate::cpu::CPU;
+
+mod cpu;
+mod parser;
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("./data/input.txt");
+    let (leftover, (directives, instructions)) = parser::parse(input).unwrap();
+    assert!(leftover.is_empty());
+
+    let mut cpu = CPU::new();
+    cpu.load_program(directives.ip_register, instructions);
+
+    while cpu.tick() {}
+
+    println!("Registers after execution: {:?}", cpu.get_registers());
 }
